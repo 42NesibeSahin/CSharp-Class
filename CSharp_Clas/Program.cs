cf9465d4ecee2961778b657b2011d3ac0d061bfc
Nesibe ŞAHİN <45272038+42Nesibe@users.noreply.github.com>
2022-10-15 17:28:12 +0300

    commit1

--- CSharp_Clas/Program.cs
+++ CSharp_Clas/Program.cs
@@ -10,6 +10,40 @@ namespace CSharp_Clas
     {
         static void Main(string[] args)
         {
+            Calisanlar kisi1 = new Calisanlar();
+            kisi1.Ad = "Nesibe";
+            kisi1.Soyad = "şahin";
+            kisi1.no = 123456;
+            kisi1.depertman = "yazılımcı";
+
+            Calisanlar kisi2 = new Calisanlar();
+            kisi2.Ad = "deneme ad";
+            kisi2.Soyad = "deneme soyad";
+            kisi2.no = 789456;
+            kisi2.depertman = "deneme depertamanı";
+
+            kisi1.CalisanBilgileri();
+            Console.WriteLine("\n*****************\n");
+            kisi2.CalisanBilgileri();
+
+
+            Console.ReadLine();
         }
     }
+     class Calisanlar
+     { 
+        public string Ad;
+        public string Soyad;
+        public int no;
+        public string depertman;
+        
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışanın Adı :" + Ad);
+            Console.WriteLine("Çalışanın Soyadı :" + Soyad);
+            Console.WriteLine("Çalışanın Numarası :" + no);
+            Console.WriteLine("Çalışanın Depertmanı :" + depertman);
+        }
+
+     }
 }
